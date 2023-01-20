fbda67a2a364369574fc040a01324cfb05b7330d
muhammed-saeed <mohammed.yahia3@gmail.com>
2023-01-20 09:51:26 +0100

    learning from domenstration

--- learn_from_domenstration/reinforce_with_imitation_learing.py
+++ learn_from_domenstration/reinforce_with_imitation_learing.py
@@ -1,4 +1,5 @@
 import gym
+import gym_gridworlds
 import numpy as np
 import torch
 import torch.nn as nn
@@ -19,9 +20,10 @@ class Policy(nn.Module):
         return x
 
 # Collect expert demonstrations
-env = gym.make('CartPole-v0')
+# env = gym.make('CartPole-v0')
+env  = gym.make('Gridworld-v0')
 expert_data = []
-for _ in range(1000):
+for _ in range(100):
     obs = env.reset()
     done = False
     while not done:
@@ -37,11 +39,12 @@ train_data = expert_data[:split]
 test_data = expert_data[split:]
 
 # Define the training loop
-policy = Policy(4, 32, 2)
+# policy = Policy(4, 32, 2)
+policy = Policy(4, 32, 4)
 criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(policy.parameters())
 
-for epoch in range(2):
+for epoch in range(1):
     running_loss = 0.0
     for obs, action in train_data:
         optimizer.zero_grad()
@@ -65,9 +68,10 @@ with torch.no_grad():
     print(f'Test Accuracy: {correct / total}')
 
 # REINFORCE with Baseline Algorithm
+# REINFORCE with Baseline Algorithm
 baseline = 0
 policy.train()
-for episode in range(10):
+for episode in range(10000):
     obs = env.reset()
     done = False
     rewards = []
@@ -75,9 +79,15 @@ for episode in range(10):
     while not done:
         action_pred = policy(torch.Tensor(obs))
         prob = nn.functional.softmax(action_pred, dim=-1)
-        action = np.random.choice(range(2), p=prob.detach().numpy()[0])
-        log_prob = torch.log(prob[0][action])
-        next_obs, reward, done, _ = env.step(action)
+        action = torch.multinomial(prob, 1).squeeze()
+        # print(action)
+        # print(prob)
+        log_prob = torch.log(prob[action.item()])
+
+
+
+
+        next_obs, reward, done, _ = env.step(action.item())
         rewards.append(reward)
         log_probs.append(log_prob)
         obs = next_obs
@@ -87,6 +97,7 @@ for episode in range(10):
     R = 0
     for r in rewards[::-1]:
         R = r + 0.99 * R
+        # print(R)
         returns.insert(0, R)
     returns = torch.Tensor(returns)
     returns = (returns - returns.mean()) / (returns.std() + 1e-8)
@@ -96,7 +107,8 @@ for episode in range(10):
     loss = 0
     for log_prob, R in zip(log_probs, returns):
         advantage = R - baseline
-        loss -= log_prob * advantage
+        loss += -log_prob * advantage
+    loss /= len(log_probs)
     loss.backward()
     optimizer.step()
 
@@ -104,3 +116,16 @@ for episode in range(10):
     baseline = 0.99 * baseline + 0.01 * returns.mean()
 
 
+# Evaluate the agent
+policy.eval()
+obs = env.reset()
+done = False
+total_reward = 0
+while not done:
+    env.render() # added this line to visualize the agent
+    action_pred = policy(torch.Tensor(obs))
+    _, action = torch.max(action_pred, 0)
+    obs, reward, done, _ = env.step(action.item())
+    total_reward += reward
+
+print(f'Total Reward: {total_reward}')
