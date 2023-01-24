22588e61c68bc4b56553d00c21b3171d0c8ae644
muhammed-saeed <mohammed.yahia3@gmail.com>
2023-01-24 16:41:31 +0100

    pg_rerinforce

---
+++ test/pg_reinforce/read_env_json.py
@@ -0,0 +1,60 @@
+import os
+import json
+
+mode = "train"
+train_path = "/home/muhammed-saeed/Documents/rl_assignments/test/task"
+train_target_path = "/home/muhammed-saeed/Documents/rl_assignments/test/solution"
+# val_path = args.val_folder
+# val_target_path = args.val_solution
+def read_env_sol_json (mode,train_path,train_target_path):
+    if(train_path and train_target_path):
+        json_files = [i for i in os.listdir(train_path) if i.endswith("json")]
+        json_target_files = [i for i in os.listdir(train_target_path) if i.endswith("json")]
+        print(json_files)
+        print(json_target_files)
+        for counter, file in enumerate(json_files):
+            # if file in json_target_files: # if train example has a solution json included
+                file_path = os.path.join(train_path, file)
+                target_path = os.path.join(train_target_path, json_target_files[counter])
+                train_data = None
+                train_seq = None
+                
+                with open(file_path) as f:
+                    train_data = json.load(f)
+                with open(target_path) as f:
+                    train_seq = json.load(f)
+                # print(train_data)
+                # print(train_data)
+                rows = train_data["gridsz_num_rows"]
+                cols = train_data["gridsz_num_cols"]
+                agent_pos = (train_data["pregrid_agent_row"], train_data["pregrid_agent_col"])
+                agent_dir = train_data["pregrid_agent_dir"]
+                agent_final_pos = (train_data["postgrid_agent_row"], train_data["postgrid_agent_col"])
+                agent_final_dir = train_data["postgrid_agent_dir"]
+                walls = train_data["walls"]
+                init_markers = train_data["pregrid_markers"]
+                final_markers = train_data["postgrid_markers"]
+                # print(type(walls[0]))
+                # print(walls)
+                walls_tuple = [tuple(sublist) for sublist in walls]
+                init_markers_tuple = [tuple(sublist) for sublist in init_markers]
+                final_markers_tuple = [tuple(sublist) for sublist in final_markers]
+                enviromentDetails =  (rows ,
+                cols ,
+                agent_pos ,
+                agent_dir ,
+                init_markers_tuple ,
+                walls_tuple ,
+                [agent_final_pos ,
+                agent_final_dir ,
+                final_markers_tuple ,
+                ],
+                ['m', 'l', 'r', 'f','pick','put']
+                )
+                print()
+                # print(train_seq["sequence"])
+                bestActionSeq = train_seq["sequence"]
+                return (enviromentDetails, bestActionSeq)
+                
+
+# print(read_env_sol_json(mode,train_path, train_target_path))
---
+++ test/pg_reinforce/reinforce_code_gridworld.py
@@ -0,0 +1,148 @@
+import argparse
+import gym
+import numpy as np
+from itertools import count
+from collections import deque
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.distributions import Categorical
+
+########
+from env import GridWorld
+from read_env_json import read_env_sol_json
+
+parser = argparse.ArgumentParser(description='PyTorch REINFORCE example')
+parser.add_argument('--gamma', type=float, default=0.99, metavar='G',
+                    help='discount factor (default: 0.99)')
+parser.add_argument('--seed', type=int, default=543, metavar='N',
+                    help='random seed (default: 543)')
+parser.add_argument('--render', default=True, action='store_true',
+                    help='render the environment')
+parser.add_argument('--log-interval', type=int, default=10, metavar='N',
+                    help='interval between training status logs (default: 10)')
+args = parser.parse_args()
+
+
+env = gym.make('CartPole-v1')
+# env.reset(seed=args.seed)
+torch.manual_seed(args.seed)
+
+mode = "train"
+train_path = "/home/muhammed-saeed/Documents/rl_assignments/test/task/task"
+train_target_path = "/home/muhammed-saeed/Documents/rl_assignments/test/task/solution"
+# m, n, init_state, orientation, markers_locations, wall_locations, terminal_state, possible_actions):
+# terminal_state #[[x,y],"orientation", [[markers1],[marker2]]]
+actions = ['m', 'l', 'r', 'f', 'pick', 'put']
+initial_settings = read_env_sol_json(mode, train_path, train_target_path)
+print(f"{initial_settings[0]} \n\n best action seq {initial_settings[1]}")
+env = GridWorld(*initial_settings[0])
+
+
+class Policy(nn.Module):
+    def __init__(self):
+        super(Policy, self).__init__()
+        self.affine1 = nn.Linear(32, 128)
+        self.dropout = nn.Dropout(p=0.6)
+        self.affine2 = nn.Linear(128, 6)
+
+        self.saved_log_probs = []
+        self.rewards = []
+
+    def forward(self, x):
+        x = self.affine1(x)
+        x = self.dropout(x)
+        x = F.relu(x)
+        action_scores = self.affine2(x)
+        return F.softmax(action_scores, dim=1)
+
+
+policy = Policy()
+optimizer = optim.Adam(policy.parameters(), lr=1e-2)
+eps = np.finfo(np.float32).eps.item()
+
+
+def select_action(state):
+    state = torch.from_numpy(state).float().unsqueeze(0)
+    probs = policy(state)
+    print(f"the probs are {probs}")
+    m = Categorical(probs)
+    action = m.sample()
+    policy.saved_log_probs.append(m.log_prob(action))
+    return action.item()
+
+
+def finish_episode():
+    R = 0
+    policy_loss = []
+    returns = deque()
+    for r in policy.rewards[::-1]:
+        R = r + args.gamma * R
+        returns.appendleft(R)
+    returns = torch.tensor(returns)
+    returns = (returns - returns.mean()) / (returns.std() + eps)
+    for log_prob, R in zip(policy.saved_log_probs, returns):
+        policy_loss.append(-log_prob * R)
+    optimizer.zero_grad()
+    policy_loss = torch.cat(policy_loss).sum()
+    policy_loss.backward()
+    optimizer.step()
+    del policy.rewards[:]
+    del policy.saved_log_probs[:]
+
+
+def main():
+    running_reward = 0
+    action_seq = []
+    eps_actions = []
+    counter = []
+    numEpisodes = 10000
+    # for i_episode in count(1):
+    for i_episode in range(numEpisodes):
+        state = env.reset()
+        ep_reward = 0
+        for t in range(1, 10000):  # Don't infinite loop while learning
+            action = select_action(state.flatten())
+            actionString = actions[action]
+            # action = actionString
+            state, reward, done, _ = env.step(actionString)
+            # print(f"{state} and action {action}")
+            # print(action)
+            # if args.render:
+            #     env.render()
+            policy.rewards.append(reward)
+            
+
+            eps_actions.append(actionString)
+            if done and reward ==0:
+                print("we are here")
+                action_seq.append(eps_actions)
+                counter.append(i_episode)
+                print(f"the action sequence is {eps_actions}")
+            if done:
+                
+                break
+          
+
+        running_reward = 0.05 * ep_reward + (1 - 0.05) * running_reward
+        finish_episode()
+        if i_episode % args.log_interval == 0:
+            print('Episode {}\tLast reward: {:.2f}\tAverage reward: {:.2f}'.format(
+                  i_episode, ep_reward, running_reward))
+        # if running_reward > env.spec.reward_threshold:
+        if reward >= 0:
+
+            print("Solved! Running reward is now {} and "
+                  "the last episode runs to {} time steps!".format(running_reward, t))
+            print(f"the action sequence is {eps_actions}")
+            break
+    
+    with open("/home/muhammed-saeed/Documents/rl_assignments/test/pg_reinforce/results.txt", "w") as fb:
+        for number, solution in enumerate(action_seq):
+            fb.write(f"The solution occured at {counter[number]} episode \n")
+            fb.write(" ".join(solution))
+            fb.write('\n-------------- -------------- ---------- -------- \n')
+
+if __name__ == '__main__':
+    main()
